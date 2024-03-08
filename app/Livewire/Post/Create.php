e21b69cf328c3bd9c06a3be46938fb1acb221e1e
RealMrHex <RealMrHex@gmail.com>
2024-03-08 17:09:58 +0330

    wire:key and share additional data to views

--- app/Livewire/Post/Create.php
+++ app/Livewire/Post/Create.php
@@ -29,7 +29,31 @@ class Create extends Component
      */
     public function bag(): array
     {
-        return [];
+        return [
+            'ip'    => request()->ip(),
+            'posts' => [
+                [
+                    'id'          => 1,
+                    'title'       => 'Title #1',
+                    'description' => 'Description #1',
+                ],
+                [
+                    'id'          => 2,
+                    'title'       => 'Title #2',
+                    'description' => 'Description #2',
+                ],
+                [
+                    'id'          => 3,
+                    'title'       => 'Title #3',
+                    'description' => 'Description #3',
+                ],
+                [
+                    'id'          => 4,
+                    'title'       => 'Title #4',
+                    'description' => 'Description #4',
+                ],
+            ],
+        ];
     }
 
     /**
--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -1,5 +1,8 @@
 <div>
     <form>
+        <small>Your ip address: <span>{{ $ip }}</span></small>
+        <br>
+        <br>
         <label>
             Title
             <br>
@@ -22,14 +25,28 @@
             ></textarea>
         </label>
     </form>
-    @if($title || $description)
-        <hr>
-        <h3>Post Preview</h3>
-        <h4>
-            I am just thinking about <span>{{ $title }}</span>
-        </h4>
-        <p>
-            {{ $description }}
-        </p>
-    @endif
+    <div>
+        @if($title || $description)
+            <hr>
+            <h3>Post Preview</h3>
+            <h4>
+                I am just thinking about <span>{{ $title }}</span>
+            </h4>
+            <p>
+                {{ $description }}
+            </p>
+        @endif
+    </div>
+    <div>
+        <h3>Recent Posts</h3>
+        <ul>
+            @foreach($posts as $post)
+                <li wire:key="recent-posts-{{ $post['id'] }}">
+                    <h4>{{ $post['title'] }}</h4>
+                    <p>{{ $post['description'] }}</p>
+                    <hr>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
